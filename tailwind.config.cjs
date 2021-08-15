de218361091b076c3d0b7e39e5adaf3e783aa5d5
Zohar Cochavi <cochavi.zohar@protonmail.com>
2021-08-15 20:56:39 +0200

    Initial commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,31 @@
+module.exports = {
+  purge : [],
+  darkMode : 'media', // or 'media' or 'class'
+  theme : {
+    extend : {},
+    textColor : {
+      light : '#000000',
+      DEFAULT : '#000000',
+      dark : '#ffffff',
+      cyan : '#26E1FA',
+    },
+    colors : {
+      transparent : 'transparent',
+      current : 'currentColor',
+      background : {
+        light : '#ffffff',
+        DEFAULT : '#ffffff',
+        dark : '#1d1d1d',
+      },
+      cyan : {
+        light : '#26E1FA',
+        DEFAULT : '#26E1FA',
+        dark : '#26E1FA',
+      },
+    }
+  },
+  variants : {
+    extend : {},
+  },
+  plugins : [],
+}
