88fce10c61efd97b87967a0a25172917fd9f65d3
Zohar Cochavi <cochavi.zohar@protonmail.com>
2021-08-24 23:52:41 +0200

    More rounded, better responsiveness in projects part, and tux :)

--- bunkernet/src/components/sliding-repo.svelte
+++ bunkernet/src/components/sliding-repo.svelte
@@ -2,7 +2,7 @@
     export let repo;
 </script>
 
-<a class="bg-lightdark p-6 shadow-xl hover:shadow-2xl transform hover:scale-105 text-center rounded-md hover:border-cyan flex flex-col gap-4 justify-center" href={`${repo.html_url}`}>
+<a target="_blank" class="bg-lightdark p-6 shadow-xl hover:shadow-2xl transform hover:scale-105 text-center rounded-xl hover:border-cyan flex flex-col gap-4 justify-center" href={`${repo.html_url}`}>
     <h2 class="uppercase text-2xl">{repo.name}</h2>
     <p>{repo.description}</p>
 </a>
