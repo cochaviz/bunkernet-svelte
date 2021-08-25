97d23977e28fdf6cb30f16eb192d8f464b693bb2
Zohar Cochavi <cochavi.zohar@protonmail.com>
2021-08-25 16:19:58 +0200

    I'm the responsiveness master...
    css becomes so ugly tho :,(

--- bunkernet/src/routes/index.svelte
+++ bunkernet/src/routes/index.svelte
@@ -10,27 +10,27 @@
 
 <!--Home--->
 
-<div class="flex flex-col justify-between border-b-2 pb-8 sm:pt-8 gap-8 border-green mx-auto mb-8">
-    <Typewriter 
-        clazz={"font-semibold text-2xl md:text-5xl"}
-        alt_color={"text-cyan dark:text-cyan"}
-        command={"echo \"Hi, my name is $(whoami)\\!\""} 
-        result={"Hi, my name is Zohar!"}
-    />
-    <div class="sm:px-16 md:pt-32">
-        <p class="sm:w-4/5">
-        I’m a Computer Science student who is very much into learning about anything! This is the place where I collect most interesting things I come accross, so feel free to have a look and see if there is anything that catches your eye! :)
+<div id="Home" class="h-screen flex flex-col justify-around border-b-2 pb-8 gap-8 border-green mx-auto mb-8">
+    <div class="h-full flex flex-col justify-evenly">
+        <Typewriter 
+            clazz={"font-semibold text-2xl md:text-5xl"}
+            alt_color={"text-cyan dark:text-cyan"}
+            command={"echo \"Hi, my name is $(whoami)\\!\""} 
+            result={"Hi, my name is Zohar!"}
+        />
+        <p class="sm:px-16 lg:w-4/5">
+        I’m a Computer Science student who is very much into learning about anything! This is the place where I collect most interesting things I come across, so feel free to have a look and see if there is anything that catches your eye! :)
         </p>
-        <div class="w-full pt-8 flex flex-row-reverse gap-4">
-            <a target="_blank" href="https://github.com/zoharcochavi"><img class="transform hover:scale-110 w-8 sm:w-12" src="/github_logo.png" alt="Github profile of Zohar Cochavi"/></a>
-            <a target="_blank" href="https://www.linkedin.com/in/zoharcochavi/"><img class="transform hover:scale-110 w-8 sm:w-12" src="/linkedin_logo.png" alt="Linkedin profile of Zohar Cochavi"/></a>
-        </div>
+    </div>
+    <div class="w-full lg:px-16 sm:pb-8 flex flex-row-reverse gap-4">
+        <a target="_blank" href="https://github.com/zoharcochavi"><img class="transform hover:scale-110 w-8 sm:w-12" src="/github_logo.png" alt="Github profile of Zohar Cochavi"/></a>
+        <a target="_blank" href="https://www.linkedin.com/in/zoharcochavi/"><img class="transform hover:scale-110 w-8 sm:w-12" src="/linkedin_logo.png" alt="Linkedin profile of Zohar Cochavi"/></a>
     </div>
 </div>
 
 <!--Projects--->
 
-<div class="flex flex-col pt-8">
+<div class="flex flex-col pt-16 sm:pt-64">
     <Typewriter 
         alt_color={"text-green dark:text-green"}
         clazz={"font-semibold text-2xl md:text-5xl"}
@@ -40,7 +40,7 @@
     />
     <div class="grid num-cols-1 sm:p-16">
         {#each $repos as repo}
-            <div class="flex gap-16 space-around sm:odd:flex-row sm:even:flex-row-reverse flex-col border-b-2 dark:border-gray border-gray-light py-16">
+            <div class="flex gap-16 space-around sm:odd:flex-row sm:even:flex-row-reverse flex-col border-b-2 dark:border-gray border-gray-light py-16 sm:py-64">
                 <img class="w-4/5 mx-auto" src={`${repo.name}.png`} alt={repo.name}/>
                 <SlidingRepo repo={repo}/>
             </div>
