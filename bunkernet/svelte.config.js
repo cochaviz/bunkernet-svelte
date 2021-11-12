8c8ded76f9cbc801df26ce53fe383e4cfb083a00
Zohar Cochavi <cochavi.zohar@protonmail.com>
2021-11-12 12:36:30 +0100

    Add static build for netlify

--- bunkernet/svelte.config.js
+++ bunkernet/svelte.config.js
@@ -1,15 +1,23 @@
 /** @type {import('@sveltejs/kit').Config} */
-import node from '@sveltejs/adapter-node';
+import adapterNode from '@sveltejs/adapter-node';
+import adapterStatic from '@sveltejs/adapter-static';
 
 const config = {
   kit : {
     // hydrate the <div id="svelte"> element in src/app.html
     target : '#svelte',
-    adapter : node({
-      out : 'build',
-      precompress : false,
-      env : {host : 'HOST', port : 'PORT'}
-    }),
+    adapter : // [
+      // adapterNode({
+        // out : 'build',
+        // precompress : false,
+        // env : {host : 'HOST', port : 'PORT'}
+      // }),
+      adapterStatic({
+        pages : 'static-build',
+        assets : 'static-build',
+        fallback : null
+      }),
+    // ]
     vite : {
       resolve : {
         alias : [
