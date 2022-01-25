952231aa9c0a96c4a292d893c9408ac27854537d
Zohar Cochavi <cochavi.zohar@protonmail.com>
2022-01-25 14:16:13 +0100

    Update build config and remove old config

--- bunkernet/svelte.config.js
+++ bunkernet/svelte.config.js
@@ -7,7 +7,7 @@ const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
-		adapter: adapterNode(),
+		adapter: adapterStatic(),
 		vite: {
 			resolve: {
 				alias: [
