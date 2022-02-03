8f6fe708e25e9cbb364f85e40d39ac5d9cf0e663
Zohar Cochavi <cochavi.zohar@protonmail.com>
2022-02-03 22:33:34 +0100

    Other remaining ui tweaks

--- bunkernet/src/components/sliding-repo.svelte
+++ bunkernet/src/components/sliding-repo.svelte
@@ -1,9 +1,11 @@
 <script>
-    export let repo;
+	export let repo;
 </script>
 
-<a target="_blank" class="no-underline bg-gray-light dark:bg-gray p-6 shadow-xl hover:shadow transform hover:scale-105 text-center rounded-xl hover:border-cyan flex flex-col gap-4 justify-center" href={`${repo.html_url}`}>
-    <h2 class="uppercase text-2xl">{repo.name}</h2>
-    <p>{repo.description}</p>
+<a
+	class="no-underline bg-gray-light dark:bg-gray p-6 shadow-xl hover:shadow transform hover:scale-105 text-center rounded-xl hover:border-cyan flex flex-col gap-4 justify-center"
+	href={`${repo.html_url}`}
+>
+	<h2 class="uppercase text-2xl">{repo.name}</h2>
+	<p>{repo.description}</p>
 </a>
-
