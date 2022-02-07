8e52a2f6f87e728316b98faf12eb9d1b0c4c863d
Zohar Cochavi <cochavi.zohar@protonmail.com>
2022-02-07 11:23:58 +0100

    Fix more dark mode issues

--- bunkernet/src/components/post-preview-small.svelte
+++ bunkernet/src/components/post-preview-small.svelte
@@ -2,9 +2,9 @@
 	export let post;
 </script>
 
-<a class="no-underline list-none p-6 rounded-md hover:bg-gray" href={`/blog/${post.postid}`}>
-	<h2 class="uppercase text-2xl text-left m-2">> {post.data.title}</h2>
-	<h3 class="text-dark m-2">{post.data.date}</h3>
+<a class="no-underline list-none hover:border-black p-6 rounded-md border-2" href={`/blog/${post.postid}`}>
+	<h2 class="uppercase text-2xl text-left m-2"> {post.data.title}</h2>
+	<h3 class="text-light dark:text-dark m-2">{post.data.date}</h3>
 	<div class="flex">
 		{#each post.data.tags as tag}
 			<p class="mx-2 text-lg text-unselected">
